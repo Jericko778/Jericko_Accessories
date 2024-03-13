b9a53670ef2a4f0f563ab762cca1502d1a5cc9f9
Jericko <jerichojohn778@gmail.com>
2024-03-12 20:24:05 -0500

    Created Lightning Boots

--- src/main/java/net/jericko/accessories/item/custom/SpeedItem.java
+++ src/main/java/net/jericko/accessories/item/custom/SpeedItem.java
@@ -1,16 +1,10 @@
 package net.jericko.accessories.item.custom;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
