4bf4d3d67852fe10110a0e2fd8d27e2331be74ac
Jericko John Veslin <153257147+Jericko778@users.noreply.github.com>
2024-09-13 00:45:07 -0500

    test and adding cursed ball assets

--- src/main/java/net/jericko/accessories/item/custom/DoubleJumpItem.java
+++ src/main/java/net/jericko/accessories/item/custom/DoubleJumpItem.java
@@ -2,10 +2,15 @@ package net.jericko.accessories.item.custom;
 
 import net.jericko.accessories.Accessories;
 import net.minecraft.client.Minecraft;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.Mth;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -26,6 +31,13 @@ public class DoubleJumpItem extends Item implements ICurioItem {
         if(uses == 0 && slotContext.entity().onGround()){
             uses = 1;
         }
+    }
+
+    @Override
+    public InteractionResultHolder<ItemStack> use(Level p_41432_, Player p_41433_, InteractionHand p_41434_) {
+        p_41432_.playSound(null, p_41433_.blockPosition(), SoundEvents.AMETHYST_CLUSTER_HIT, SoundSource.PLAYERS, 1.0F, 1.0F);
+
+        return super.use(p_41432_, p_41433_, p_41434_);
     }
 
     @Override
