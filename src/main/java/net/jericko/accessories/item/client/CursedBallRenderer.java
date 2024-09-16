fc71e291926401a7b76d7567090355ca21609c00
Jericko John Veslin <153257147+Jericko778@users.noreply.github.com>
2024-09-16 18:44:11 -0500

    8 Ball Added

--- src/main/java/net/jericko/accessories/item/client/CursedBallRenderer.java
+++ src/main/java/net/jericko/accessories/item/client/CursedBallRenderer.java
@@ -1,9 +1,10 @@
 package net.jericko.accessories.item.client;
 
 import net.jericko.accessories.item.custom.ArmItem;
+import net.jericko.accessories.item.custom.SpinBallItem;
 import software.bernie.geckolib.renderer.GeoItemRenderer;
 
-public class CursedBallRenderer extends GeoItemRenderer<ArmItem> {
+public class CursedBallRenderer extends GeoItemRenderer<SpinBallItem> {
 
     public CursedBallRenderer() {
         super(new CursedBallModel());
--- src/main/java/net/jericko/accessories/item/custom/SpinBallItem.java
+++ src/main/java/net/jericko/accessories/item/custom/SpinBallItem.java
@@ -2,6 +2,7 @@ package net.jericko.accessories.item.custom;
 
 import com.mojang.datafixers.util.Either;
 import net.jericko.accessories.entity.KnuckleBlasterExplosion;
+import net.jericko.accessories.item.client.CursedBallRenderer;
 import net.jericko.accessories.item.client.KnuckleblasterRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BlockEntityWithoutLevelRenderer;
@@ -11,7 +12,12 @@ import net.minecraft.core.HolderOwner;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.stats.Stats;
 import net.minecraft.tags.TagKey;
+import net.minecraft.util.Mth;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.damagesource.DamageSource;
@@ -19,10 +25,15 @@ import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.ai.targeting.TargetingConditions;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.projectile.ThrownTrident;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TridentItem;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
@@ -43,8 +54,7 @@ import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
-public class SpinBallItem extends Item implements GeoItem {
-    //So far only added animation of the item
+public class SpinBallItem extends TridentItem implements GeoItem {
     private final AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
 
     public SpinBallItem(Properties p_41383_) {
@@ -52,17 +62,41 @@ public class SpinBallItem extends Item implements GeoItem {
     }
 
     private PlayState predicate(AnimationState animationState) {
-        if(Minecraft.getInstance().player.isShiftKeyDown() && !animationState.getController().getAnimationState().equals(AnimationController.State.RUNNING)) {
-            animationState.getController().setAnimation(RawAnimation.begin().then("punch", Animation.LoopType.PLAY_ONCE));
-            animationState.resetCurrentAnimation();
-        }
+            animationState.getController().setAnimation(RawAnimation.begin().then("animation.model.spin", Animation.LoopType.LOOP));
+
         return PlayState.CONTINUE;
     }
 
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand p_41434_) {
+    public void releaseUsing(ItemStack p_43394_, Level p_43395_, LivingEntity p_43396_, int p_43397_) {
+        if (p_43396_ instanceof Player player) {
+            int i = this.getUseDuration(p_43394_) - p_43397_;
+            if (i >= 10) {
+                    if (!p_43395_.isClientSide) {
+                        p_43394_.hurtAndBreak(1, player, (p_43388_) -> {
+                            p_43388_.broadcastBreakEvent(p_43396_.getUsedItemHand());
+                        });
+                            ThrownTrident throwntrident = new ThrownTrident(p_43395_, player, p_43394_);
+                            throwntrident.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, 2.5F + (float) 0.0F, 1.0F);
+                            if (player.getAbilities().instabuild) {
+                                throwntrident.pickup = AbstractArrow.Pickup.CREATIVE_ONLY;
+                            }
 
-        return super.use(level, player, p_41434_);
+                            p_43395_.addFreshEntity(throwntrident);
+                            p_43395_.playSound((Player) null, throwntrident, SoundEvents.TRIDENT_THROW, SoundSource.PLAYERS, 1.0F, 1.0F);
+                            if (!player.getAbilities().instabuild) {
+                                player.getInventory().removeItem(p_43394_);
+                    }
+                }
+            }
+        }
+    }
+
+    public InteractionResultHolder<ItemStack> use(Level p_43405_, Player p_43406_, InteractionHand p_43407_) {
+        ItemStack itemstack = p_43406_.getItemInHand(p_43407_);
+
+            p_43406_.startUsingItem(p_43407_);
+            return InteractionResultHolder.consume(itemstack);
     }
 
     @Override
@@ -83,12 +117,12 @@ public class SpinBallItem extends Item implements GeoItem {
     @Override
     public void initializeClient(Consumer<IClientItemExtensions> consumer) {
         consumer.accept(new IClientItemExtensions() {
-            private KnuckleblasterRenderer renderer;
+            private CursedBallRenderer renderer;
 
             @Override
             public BlockEntityWithoutLevelRenderer getCustomRenderer() {
                 if (this.renderer == null) {
-                    renderer = new KnuckleblasterRenderer();
+                    renderer = new CursedBallRenderer();
                 }
 
                 return this.renderer;
